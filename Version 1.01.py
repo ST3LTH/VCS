3e15d165f6e0c5fa0811754de85cf21d0b288cd4
ST3LTH <marquardtcameron@gmail.com>
2024-11-24 21:39:43 -0500

    Add files via upload

---
+++ Version 1.01.py
@@ -0,0 +1,7 @@
+def calculate_sum(numbers):
+    """Calculate the sum of a list of numbers."""
+    return sum(numbers)
+
+# Example usage:
+nums = [1, 2, 3, 4, 5]
+print("Sum is", calculate_sum(nums))
